c7bfe6f342b30973849f1f75ebb2285d28e3480f
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-04-24 21:11:19 -0400

    Update and rename index.php to home.php

--- home.php
+++ home.php
@@ -40,7 +40,7 @@ session_start();
             <!-- Website logo -->
             <li><img class="logo" src="./assets/images/logo.png"/> </li>
             <!-- Navigation links -->
-            <li><a href="index.php">Home</a></li>
+            <li><a href="home.php">Home</a></li>
             <li><a href="albums.php">Albums</a></li>
         </ul>
 
