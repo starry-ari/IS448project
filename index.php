317fb85f49bbb696f1bf91fc400438381b3ba286
Arianna <44093846+starry-ari@users.noreply.github.com>
2026-05-21 11:48:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,115 +1,73 @@
-<?php session_start();?>
-<!-- Arianna Richardson
-IS 448 High Fidelity project
- home.html file
--->
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
- 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--Website logo-->
-        <link rel="icon" type="image/x-icon" href="./assets/images/logo.png">
-    <!-- Title -->
+    <link rel="icon" type="image/x-icon" href="./assets/images/logo.png">
     <title>High Fidelity</title>
-
-    <!-- jQuery CDN to use fetch()-->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <!-- Link to the external JavaScript file -->
     <script src="./main.js"></script>
-    
-
-    <!-- Link to the external CSS stylesheet -->
-    <link rel="stylesheet" href="public/src/style.css">
+    <!-- FIXED: CSS path was pointing to public/src/style.css but all files
+         are served from public/src/ so the path should be relative -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-    <!-- Navigation bar -->
-  
-    <?php 
-  if (isset($_SESSION['username'])) { 
-  
-     echo   '<div class="navbar">
-     
-     <ul>
-            <!-- Website logo -->
-            <li><img class="logo" src="./assets/images/logo.png"/> </li>
-            <!-- Navigation links -->
-            <li><a href="index.php">Home</a></li>
-            <li><a href="/albums.php">Albums</a></li>
-        </ul>
 
-        <!-- Search bar -->
-        <div class="search">
-            <input type="text" id="searchbox" placeholder="Search for albums here...  " onkeypress="handleKeyPress(event)">
-        </div>
-
- 
-     <img id="pfp" src="./assets/images/pfp.png" alt="Profile Picture"> <a id="Hsignin" href="./Profilepage.php">' . htmlspecialchars($_SESSION['username']) . '</a>
-    <a href="logout.php">Logout</a>
-    </div>';
-  } else { 
-  
-      echo     '<div class="navbar"> <ul>
-      <!-- Website logo -->
-      <li><img class="logo" src="./assets/images/logo.png"/> </li>
-      <!-- Navigation links -->
+<!-- Navigation bar -->
+<?php if (isset($_SESSION['username'])): ?>
+  <div class="navbar">
+    <ul>
+      <li><img class="logo" src="./assets/images/logo.png"/></li>
       <li><a href="index.php">Home</a></li>
-      <li><a href="CreateProfile.html" >Create Account</a></li>
       <li><a href="albums.php">Albums</a></li>
-  </ul>
-
-  <!-- Search bar -->
-  <div class="search">
-      <input type="text" id="searchbox" placeholder=" Search for albums here... " onkeypress="handleKeyPress(event)">
+    </ul>
+    <div class="search">
+      <input type="text" id="searchbox" placeholder="Search for albums here..." onkeypress="handleKeyPress(event)">
+    </div>
+    <img id="pfp" src="./assets/images/pfp.png" alt="Profile Picture">
+    <a id="Hsignin" href="./Profilepage.php"><?php echo htmlspecialchars($_SESSION['username']); ?></a>
+    <a href="logout.php">Logout</a>
   </div>
 
-
-<img id="pfp" src="./assets/images/pfp.png" alt="Profile Picture">
-
-       <a id="Hsignin" href="./AccountLogin.html">Sign In</a>
-       </div>';
-  }
-  ?>
-
-
-    <!-- Front page -->
-    <div id="front">
-        <h2 id="homeText">
-            Review your favorite albums.
-            <br>
-            Share your playlists with friends.
-            <br>
-            Discover new artists.
-        </h2>
-        <!-- Get Started button -->
-       <a href="albums.php"> <button >GET STARTED</button></a>
+<?php else: ?>
+  <div class="navbar">
+    <ul>
+      <li><img class="logo" src="./assets/images/logo.png"/></li>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="CreateProfile.html">Create Account</a></li>
+      <li><a href="albums.php">Albums</a></li>
+    </ul>
+    <div class="search">
+      <input type="text" id="searchbox" placeholder="Search for albums here..." onkeypress="handleKeyPress(event)">
     </div>
+    <img id="pfp" src="./assets/images/pfp.png" alt="Profile Picture">
+    <a id="Hsignin" href="./AccountLogin.html">Sign In</a>
+  </div>
+<?php endif; ?>
 
-   
-    <script>
+<!-- Front page -->
+<div id="front">
+    <h2 id="homeText">
+        Review your favorite albums.
+        <br>
+        Share your playlists with friends.
+        <br>
+        Discover new artists.
+    </h2>
+    <a href="albums.php"><button>GET STARTED</button></a>
+</div>
 
-         
-      
-        document.querySelector("#searchbox").addEventListener("keypress", function (event) {
-            if (event.key === 'Enter') {
-              // Get the value of the input field
-              const searchTerm = document.querySelector("#searchbox").value;
-          
-              // Store the search term in localStorage
-              localStorage.setItem('term', searchTerm);
-          
-              // Redirect to the album results page
-              window.location.href = './album-results.php';
-            }
-          });
-          
-                
-               
-  
-    </script>
-     
+<script>
+  document.querySelector("#searchbox").addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
+      const searchTerm = document.querySelector("#searchbox").value;
+      localStorage.setItem('term', searchTerm);
+      window.location.href = './album-results.php';
+    }
+  });
+</script>
+
 </body>
 </html>
